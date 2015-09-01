baecf62fc0931ee6c96c2b9eb3f989696b70eb86
rzymek <rzymek@gmail.com>
2015-09-01 16:48:17 +0200

    GET_LAYERS

--- mirror/src/main/java/slippyWMTS/GUI.java
+++ mirror/src/main/java/slippyWMTS/GUI.java
@@ -14,6 +14,7 @@ import javax.swing.JTextArea;
 public class GUI extends JFrame implements Runnable, StatusListener {
 
 	private static final int MAX_LINES = 10000;
+	private static final int GET_LAYERS = 6;//10;
 	private JTextArea log;
 	private JProgressBar status;
 	private Thread thread;
@@ -50,9 +51,10 @@ public class GUI extends JFrame implements Runnable, StatusListener {
 		thread = new Thread(this) {
 			public void run() {
 				String endpoint = "http://mapy.geoportal.gov.pl/wss/service/WMTS/guest/wmts/TOPO";
-				String[] sets = { ".*EPSG:.*:2180", ".*EPSG:.*:4326" };
+//				String[] sets = { ".*EPSG:.*:2180", ".*EPSG:.*:4326" };
+				String[] sets = { ".*EPSG:.*:4326" };
 				for (String set : sets) {
-					for (int i = 0; i <= 10; i++) {
+					for (int i = 0; i <= GET_LAYERS; i++) {
 						try {
 							final Fetch fetcher = new Fetch(endpoint, i, set) {
 								protected void error(String msg) throws IOException {
