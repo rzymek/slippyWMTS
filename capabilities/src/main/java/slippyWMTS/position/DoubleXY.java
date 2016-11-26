2665e9c82832cca587c7458ab77332300fc716f8
rzymek <rzymek@gmail.com>
2016-11-26 22:11:41 +0100

    DoubleXY improvements

--- capabilities/src/main/java/slippyWMTS/position/DoubleXY.java
+++ capabilities/src/main/java/slippyWMTS/position/DoubleXY.java
@@ -1,11 +1,27 @@
 package slippyWMTS.position;
 
-public class DoubleXY {
-  public double x;
-  public double y;
+import java.text.DecimalFormat;
 
-  @Override
-  public String toString() {
-    return x+","+y;
-  }
+public class DoubleXY {
+    public double x;
+    public double y;
+
+    public DoubleXY() {
+    }
+
+    public DoubleXY(double[] xy) {
+        this.x = xy[0];
+        this.y = xy[1];
+    }
+
+    public DoubleXY(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat fmt = new DecimalFormat("0.00");
+        return fmt.format(x) + "," + fmt.format(y);
+    }
 }
