4c79dfa365a92e31df110845079728560d2a6148
rzymek <rzymek@gmail.com>
2016-12-02 08:00:39 +0100

    metadata

--- batch-convert/src/main/resources/mbtiles-ddl.sql
+++ batch-convert/src/main/resources/mbtiles-ddl.sql
@@ -12,3 +12,9 @@ CREATE UNIQUE INDEX IF NOT EXISTS tile_index on tiles
 PRAGMA synchronous=0;
 PRAGMA locking_mode=EXCLUSIVE;
 PRAGMA journal_mode=DELETE;
+
+insert or ignore into metadata(name,value) values ('name','topo');
+insert or ignore into metadata(name,value) values ('type','baselayer');
+insert or ignore into metadata(name,value) values ('version','2');
+insert or ignore into metadata(name,value) values ('description','geoportal');
+insert or ignore into metadata(name,value) values ('format','jpg');
